fc663ba564e0e7f51f08ca414795a0a918b1ec0f
Divya <divzivofficial@gmail.com>
2023-02-14 11:11:20 +0530

    resume

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html");
 });
 
+app.get("/resume", function(req, res){
+	res.sendFile(__dirname+"/resume.html");
+});
+
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
