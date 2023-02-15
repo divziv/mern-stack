7a8f10957c31c88637dfe9fe952d37c7f0253f7f
Divya <divzivofficial@gmail.com>
2023-02-15 21:56:23 +0530

    db connected

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const mongoose=require("mongoose");
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -22,7 +25,18 @@ app.get("/card", function(req, res){
 app.get("/pics", function(req, res){
 	res.sendFile(__dirname+"/pics.html");
 });
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+	if(err)
+	{
+		console.error(err);
+	}
+	else{
+		console.log("DB Connected.");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});		
+	}
+
 });
