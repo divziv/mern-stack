c9e63b8eddaffcc2cebee721167d094a81e6b869
Divya <divzivofficial@gmail.com>
2023-02-23 21:35:22 +0530

    making changes

--- backend/lib/userlib.js
+++ backend/lib/userlib.js
@@ -1,6 +1,8 @@
-const userModel = require("../models/userModel");
+//const userModel = require("../models/userModel");
 
-module.exports.getAllUsers = async function(callback)
+import userModel from "../models/userModel.js"
+
+export async function getAllUsers(callback)
 {
     try{
         var users = await userModel.find({isDeleted:false});
@@ -11,7 +13,7 @@ module.exports.getAllUsers = async function(callback)
     }
 }
 
-module.exports.createFirstUser = async function(callback)
+export async function createFirstUser(callback)
 {
     try{
         var user = {
@@ -27,7 +29,7 @@ module.exports.createFirstUser = async function(callback)
     }
 }
 
-module.exports.createUser = async function(user,callback)
+export async function createUser(user,callback)
 {
     try{
         var newUser =  new userModel(user);
@@ -40,7 +42,7 @@ module.exports.createUser = async function(user,callback)
 }
 
 
-module.exports.updateUser = async function(name,data,callback)
+export async function updateUser(name,data,callback)
 {
     try{
         var query = {
@@ -53,7 +55,7 @@ module.exports.updateUser = async function(name,data,callback)
         callback(err,null);
     }
 }
-module.exports.getUserByFilter = async function(filter,callback)
+export async function getUserByFilter(filter,callback)
 {
     try{
         var result=await userModel.findOne(filter);
@@ -63,7 +65,7 @@ module.exports.getUserByFilter = async function(filter,callback)
         callback(err,null);
     }
 }
-module.exports.deleteUser = async function(name, callback)
+export async function deleteUser(name, callback)
 {
     try{
         var query = {
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-require("dotenv").config();
-const userlib=require("./backend/lib/userlib");
+//require("dotenv").config();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+//const userlib=require("./backend/lib/userlib");
+
+import userlib from "./backend/lib/userlib.js";
+
 const todolib=require("./backend/lib/todolib");
 const mongoose=require("mongoose");
-const express = require('express');
+const {request} = require('express');
+
 const app = express();
 const port = process.env.PORT || 5010;
 const options={
